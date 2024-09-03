e7207e202ffc0ff8d52dc5b2fdd55ae08e1cd98d
Hari Krishnan <harikrishnan@Haris-MacBook-Air.local>
2024-09-03 17:23:58 +0530

    base

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -3,7 +3,7 @@ import socketIOClient from 'socket.io-client';
 
 const ENDPOINT =
   window.location.host.indexOf('localhost') >= 0
-    ? 'http://127.0.0.1:5000'
+    ? 'http://127.0.0.1:4000'
     : window.location.host;
 
 export default function ChatBox(props) {
